076222e1a80e75ffacc6fdbf7456f5acc275bd38
Daniel Brígida <danielbrigida@MacBook-Air-de-Daniel.local>
2025-08-08 18:46:30 -0300

    feat: component list

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,14 +1,26 @@
+import { useState } from 'react';
 import { GroupCard } from '../../components/GroupCard';
 import { Header } from '../../components/Header';
 import { Highlight } from '../../components/Highlight';
 import { Container } from './styles';
+import { FlatList } from 'react-native';
 
 export function Groups() {
+  const [groups, setGroups] = useState([
+    'Galera da Rocket',
+    'Amigos',
+    'Família',
+  ]);
+
   return (
     <Container>
       <Header showBackButton />
       <Highlight title="Turmas" subTitle="jogue com a sua turma" />
-      <GroupCard title="Galera do Ignite" />
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <GroupCard title={item} />}
+      />
     </Container>
   );
 }
