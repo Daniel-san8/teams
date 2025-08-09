2e21dbf43bd0e652e8e73c102eb422ce9ba74ae8
Daniel Brígida <danielbrigida@MacBook-Air-de-Daniel.local>
2025-08-08 23:48:54 -0300

    feat: add theme

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,8 +1,11 @@
 import { TextInputProps } from 'react-native';
 import { Container } from './styles';
+import { useTheme } from 'styled-components/native';
 
 type Props = TextInputProps;
 
 export function Input({ ...rest }: Props) {
-  return <Container {...rest} />;
+  const { COLORS } = useTheme();
+
+  return <Container placeholderTextColor={COLORS.GRAY_300} {...rest} />;
 }
