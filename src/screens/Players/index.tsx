d61dc9c22de1b393c0821ea58af2870406860d14
Daniel Brígida <danielbrigida@MacBook-Air-de-Daniel.local>
2025-08-12 17:39:56 -0300

    feat: player card

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -6,10 +6,25 @@ import { Highlight } from '../../components/Highlight';
 import { Input } from '../../components/Input';
 import { Container, Form, HeaderList, NumberOfPlayers } from './styles';
 import { useState } from 'react';
+import { PlayerCard } from '../../components/PlayerCard';
+import { ListEmpty } from '../../components/ListEmpty';
+import { Button } from '../../components/Button';
 
 export function Players() {
   const [team, setTeam] = useState('Time A');
-  const [players, setPlayers] = useState(['Daniel', 'Rodrigo', 'Diego']);
+  const [players, setPlayers] = useState([
+    'Daniel',
+    'Rodrigo',
+    'Diego',
+    'Mayk',
+    'Vini',
+    'Ana',
+    'Biro',
+    'Fernando',
+    'Gabriel',
+    'Maria',
+    'João',
+  ]);
   return (
     <Container>
       <Header showBackButton />
@@ -38,6 +53,24 @@ export function Players() {
         />
         <NumberOfPlayers>{players.length}</NumberOfPlayers>
       </HeaderList>
+
+      <FlatList
+        data={players}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <PlayerCard name={item} onRemove={() => undefined} />
+        )}
+        ListEmptyComponent={() => (
+          <ListEmpty message="Não há pessoas nesse time" />
+        )}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={[
+          { paddingBottom: 100 },
+          players.length === 0 && { flex: 1 },
+        ]}
+      />
+
+      <Button title="Remover Turma" type="SECONDARY" />
     </Container>
   );
 }
