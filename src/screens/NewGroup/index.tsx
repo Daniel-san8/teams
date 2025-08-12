8b4d242cb738a1284472365e8f4495d7457271d5
Daniel Brígida <danielbrigida@MacBook-Air-de-Daniel.local>
2025-08-12 18:33:12 -0300

    feat: navigate

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,3 +1,4 @@
+import { useNavigation } from '@react-navigation/native';
 import { Button } from '../../components/Button';
 import { Header } from '../../components/Header';
 import { Highlight } from '../../components/Highlight';
@@ -5,6 +6,11 @@ import { Input } from '../../components/Input';
 import { Container, Content, Icon } from './styles';
 
 export function NewGroup() {
+  const navigation = useNavigation();
+
+  function handleNew() {
+    navigation.navigate('players', { group: 'Rocket' });
+  }
   return (
     <Container>
       <Header showBackButton />
@@ -17,7 +23,7 @@ export function NewGroup() {
           subTitle="crie a turma para adicionar as pessoas"
         />
         <Input placeholder="Nome da turma" />
-        <Button title="Criar" style={{ marginTop: 20 }} />
+        <Button title="Criar" style={{ marginTop: 20 }} onPress={handleNew} />
       </Content>
     </Container>
   );
