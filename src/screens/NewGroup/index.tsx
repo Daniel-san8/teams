e307e8ef5a421e8e33c3004798a265858089f283
Daniel Brígida <danielbrigida@MacBook-Air-de-Daniel.local>
2025-08-12 18:47:08 -0300

    feat: use params

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -4,12 +4,14 @@ import { Header } from '../../components/Header';
 import { Highlight } from '../../components/Highlight';
 import { Input } from '../../components/Input';
 import { Container, Content, Icon } from './styles';
+import { useState } from 'react';
 
 export function NewGroup() {
+  const [group, setGroup] = useState('');
   const navigation = useNavigation();
 
   function handleNew() {
-    navigation.navigate('players', { group: 'Rocket' });
+    navigation.navigate('players', { group });
   }
   return (
     <Container>
@@ -22,7 +24,7 @@ export function NewGroup() {
           title="Nova turma"
           subTitle="crie a turma para adicionar as pessoas"
         />
-        <Input placeholder="Nome da turma" />
+        <Input placeholder="Nome da turma" onChangeText={setGroup} />
         <Button title="Criar" style={{ marginTop: 20 }} onPress={handleNew} />
       </Content>
     </Container>
--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -9,30 +9,24 @@ import { useState } from 'react';
 import { PlayerCard } from '../../components/PlayerCard';
 import { ListEmpty } from '../../components/ListEmpty';
 import { Button } from '../../components/Button';
+import { useRoute } from '@react-navigation/native';
+
+type RouteParams = {
+  group: string;
+};
 
 export function Players() {
   const [team, setTeam] = useState('Time A');
-  const [players, setPlayers] = useState([
-    'Daniel',
-    'Rodrigo',
-    'Diego',
-    'Mayk',
-    'Vini',
-    'Ana',
-    'Biro',
-    'Fernando',
-    'Gabriel',
-    'Maria',
-    'João',
-  ]);
+  const [players, setPlayers] = useState([]);
+
+  const route = useRoute();
+  const { group } = route.params as RouteParams;
+
   return (
     <Container>
       <Header showBackButton />
 
-      <Highlight
-        title="Nome da turma"
-        subTitle="adicione a galera e separe os times"
-      />
+      <Highlight title={group} subTitle="adicione a galera e separe os times" />
       <Form>
         <Input autoCorrect={false} placeholder="Nome do participante" />
 
