29d4f5789c60268fb7d8d75a85ba79d497b01f0d
Daniel Brígida <danielbrigida@MacBook-Air-de-Daniel.local>
2025-08-12 15:24:28 -0300

    feat: create buttons

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,11 +1,15 @@
+import { FlatList } from 'react-native';
 import { ButtonIcon } from '../../components/ButtonIcon';
 import { Filter } from '../../components/Filter';
 import { Header } from '../../components/Header';
 import { Highlight } from '../../components/Highlight';
 import { Input } from '../../components/Input';
-import { Container, Form } from './styles';
+import { Container, Form, HeaderList, NumberOfPlayers } from './styles';
+import { useState } from 'react';
 
 export function Players() {
+  const [team, setTeam] = useState('Time A');
+  const [players, setPlayers] = useState(['Daniel', 'Rodrigo', 'Diego']);
   return (
     <Container>
       <Header showBackButton />
@@ -19,8 +23,21 @@ export function Players() {
 
         <ButtonIcon icon="add" />
       </Form>
-
-      <Filter title="Time A" isActive />
+      <HeaderList>
+        <FlatList
+          data={['Time A', 'Time B']}
+          keyExtractor={(item) => item}
+          renderItem={({ item }) => (
+            <Filter
+              title={item}
+              isActive={item === team}
+              onPress={() => setTeam(item)}
+            />
+          )}
+          horizontal
+        />
+        <NumberOfPlayers>{players.length}</NumberOfPlayers>
+      </HeaderList>
     </Container>
   );
 }
