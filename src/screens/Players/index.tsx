b6ad65ef7fec1d0ca15d3ee5f15849f784c631b6
Daniel Brígida <danielbrigida@MacBook-Air-de-Daniel.local>
2025-08-12 12:52:50 -0300

    feat: input

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,7 +1,8 @@
 import { ButtonIcon } from '../../components/ButtonIcon';
 import { Header } from '../../components/Header';
 import { Highlight } from '../../components/Highlight';
-import { Container } from './styles';
+import { Input } from '../../components/Input';
+import { Container, Form } from './styles';
 
 export function Players() {
   return (
@@ -12,8 +13,11 @@ export function Players() {
         title="Nome da turma"
         subTitle="adicione a galera e separe os times"
       />
+      <Form>
+        <Input autoCorrect={false} placeholder="Nome do participante" />
 
-      <ButtonIcon />
+        <ButtonIcon icon="add" />
+      </Form>
     </Container>
   );
 }
